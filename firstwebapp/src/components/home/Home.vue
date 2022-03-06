7accbf447dcebf6515140aad804ab63a3873bcb0
Feng-Mumu <1084840615@qq.com>
2022-03-06 13:14:08 +0800

    header

---
+++ firstwebapp/src/components/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <HomeHeader/>
+    </div>
+</template>
+<script>
+import HomeHeader from './pages/Header'
+export default {
+    components:{
+        HomeHeader
+    }
+}
+</script>
+<style type="text/css"></style>
