5f09e467cd3db99dcf09d1e65a69669db89c6918
Feng-Mumu <1084840615@qq.com>
2022-03-21 16:56:39 +0800

    add scroll

--- firstwebapp/src/components/city/pages/List.vue
+++ firstwebapp/src/components/city/pages/List.vue
@@ -1,32 +1,145 @@
 <template>
-    <div class="list">
-        <div
-        v-for ='(val,key) in cities'
-        >
-            <div class="list-title">
-            {{key}}
+    <div ref="container" class="container">
+        <div class="content">
+            <!--hot-->
+            <div class="hot">
+                <div class="hot-title">
+                    热门城市
+                </div>
+                <ul class="hot-list">
+                    <li class="hot-item"
+                    v-for ='item in hotCities' 
+                    :key = 'item.id'
+                    >
+                        {{item.name}}
+                    </li>
+                </ul>
             </div>
-            <ul class="list-msg">
-                <li class="list-item"
-                v-for="item in val"
-                :key = "item.id"
+
+            <!--sort-->
+            <div class="sort">
+                <div class="sort-title">
+                    字母排序
+                </div>
+                <ul class="sort-list">
+                    <li class="sort-item"
+                    v-for="(val,key) in cities"
+                    >
+                    {{key}}
+                    </li>
+                </ul>
+            </div>
+
+            <!--list-->
+            <div class="list">
+                <div
+                v-for ='(val,key) in cities'
                 >
-                    {{item.name}}
-                </li>
-                
-            </ul> 
+                    <div class="list-title">
+                    {{key}}
+                    </div>
+                    <ul class="list-msg">
+                        <li class="list-item"
+                        v-for="item in val"
+                        :key = "item.id"
+                        >
+                            {{item.name}}
+                        </li>
+                        
+                    </ul> 
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-    props:['cities']
+	props:['hotCities','cities'],
+	data () {
+		return {
+			scroll:''
+		}
+	},
+	mounted(){
+		
+		let container = this.$refs['container'];
+		this.scroll = new BScroll(container)
+	},
 }
+
 </script>
 
 <style lang="stylus" scoped>
 @import '~css/common.tyle';
+
+.container{
+    position: absolute;
+	overflow:hidden;
+	left:0;
+	right:0;
+	bottom:0;
+	top:.88rem;
+	background: #f5f5f5;
+}
+.content{
+      height 12594px
+}
+
+.hot-title{
+    font-size: .22rem;
+    padding: .2rem .3rem;
+    color:#212121;
+}
+.hot-list{
+    position: relative;
+    overflow: hidden;
+    background: #fff;
+    font-size: .28rem;
+    color: #212121;
+}
+.hot-list:before{
+    content:" ";
+    position: absolute;
+    height: 100%;
+    left: 33.33%;
+    right: 33.33%;
+    border-left:.02rem solid #ddd;
+    border-right:.02rem solid #ddd  ;
+}
+
+.hot-item{
+    height: .9rem;
+    text-align: center;
+    line-height: .9rem;
+    float: left;
+    width:33.333333%;
+    border-bottom:.02rem solid #ddd ;
+}
+
+.sort-title{
+    font-size: .22rem;
+    padding: .2rem .3rem;
+    color:#212121;
+}
+.sort-list{
+    position: relative;
+    overflow: hidden;
+    background: #fff;
+    font-size: .28rem;
+    color: #212121;
+}
+
+
+.sort-item{
+    height: .9rem;
+    text-align: center;
+    line-height: .9rem;
+    float: left;
+    width:16.666666%;
+}
+
 
 .list-title{
     font-size: .22rem;
